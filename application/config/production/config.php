c96cea48c49e1b999bbdaba7e0c08dded996d936
Sachin <sachin.yadav@appsquadz.co>
2025-06-06 11:42:45 +0530

    added a .ebeextensions/ folder

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://cbams-env.eba-zexui5pf.ap-south-1.elasticbeanstalk.com/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
